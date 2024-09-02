ce9b1bdb01d80d4f9f4bd1588f482a53882d0577
stanleybengeorge <georgestanley@cityuniversity.edu>
2024-09-02 19:09:22 +0530

    HOS10A

---
+++ cityu_pack/sub_func_pack/func_module.py
@@ -0,0 +1,6 @@
+def add(x,y):
+    return x+y
+
+def avg(x,y):
+    return add(x,y)/2
+
---
+++ cityu_pack/tests/sub_func_pack/test_func_module.py
@@ -0,0 +1,6 @@
+from cityu_pack.sub_func_pack.func_module import *
+
+def test_add():
+    assert add(1,2) == 3
+
+
